e5a46a24aed6f7bf328a6919e51a5595d7a4869c
SRLambda <camilo.fuentes.01.03.20@gmail.com>
2025-04-16 21:52:44 -0400

    Arreglos profe

--- src/duenio.py
+++ src/duenio.py
@@ -1,9 +1,11 @@
 from typing import List
-from persona import Persona
 
-class Duenio(Persona):
+class Duenio():
     def __init__(self, nombre: str, rut: int, correo: str, direccion: str, numero_contacto: str):
-        super().__init__(nombre, rut, correo, direccion)
+        self.__nombre = nombre
+        self.__rut = rut
+        self.__correo = correo
+        self.__direccion = direccion
         self.__numero_contacto = numero_contacto
         self.__mascotas: List['Mascota'] = []
 
--- src/veterinario.py
+++ src/veterinario.py
@@ -1,9 +1,10 @@
 from typing import Dict
-from persona import Persona
 
-class Veterinario(Persona):
-    def __init__(self, nombre: str, rut: int, correo: str, direccion: str, especialidad: str):
-        super().__init__(nombre, rut, correo, direccion)
+class Veterinario():
+    def __init__(self, nombre: str, rut: int, correo: str, especialidad: str):
+        self._nombre = nombre
+        self._rut = rut
+        self._correo = correo
         self.__especialidad = especialidad
         self.__horarios_ocupados: Dict[str, str] = {}
 
