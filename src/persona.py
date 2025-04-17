bb872e0880b2a1e61611f0b227dc414d837d8d4c
SRLambda <camilo.fuentes.01.03.20@gmail.com>
2025-04-16 21:59:42 -0400

    aux

--- src/persona.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC
-class Persona(ABC):
-    def __init__(self, nombre: str, rut: int, correo: str, direccion: str):
-        self._nombre = nombre
-        self._rut = rut
-        self._correo = correo
-        self._direccion = direccion
